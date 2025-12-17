4735ca87bda799972d3edd9787fcaf303ad2207a
Divyansh Bhardwaj <divyanshb22@gmail.com>
2025-12-18 03:14:21 +0530

    feat: integrate Tailwind CSS into the project

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,14 @@
  */
 
 import {defineConfig} from 'vite'; // Import the `defineConfig` helper from Vite
-import react from '@vitejs/plugin-react'; // Import the official React plugin for Vite
+import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite"; // Import the official React plugin for Vite
 
 // Export the Vite configuration
 export default defineConfig({
 	plugins: [
 		// Add the React plugin to enable support for React and JSX/TSX
 		react(),
+		tailwindcss()
 	],
 });

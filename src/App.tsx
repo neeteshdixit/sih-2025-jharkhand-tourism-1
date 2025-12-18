4f063e6d62722edacc172f0274cd4790ff45f3df
Neetesh Dixit <neeteshdixit@gmail.com>
2025-12-18 11:03:49 +0530

    add header file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-const App = () => {
-	return (
-		<div>App</div>
+import './App.css'
+import {Header} from "./components/organisms/Header/Header.tsx";
+import {Footer} from "./components/organisms/Footer/Footer.tsx";
+
+function App() {
+	return(
+		<>
+			<Header/>
+			<Footer/>
+		</>
 	)
 }
-export default App
+export default App;
